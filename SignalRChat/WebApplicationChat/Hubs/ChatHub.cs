cd06bd05612e89c02c5021f8771c4e3991538166
fjvela <fjvela@gmail.com>
2018-03-15 23:10:11 +0100

    INIT.

---
+++ SignalRChat/WebApplicationChat/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+
+        public void SendNewUser(string user)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.newChatMsg($"{DateTime.Now} The {user} connected!");
+        }
+
+        public void Send(string user, string msg)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.newChatMsg($"{DateTime.Now} {user}: {msg}");
+        }
+    }
+}
