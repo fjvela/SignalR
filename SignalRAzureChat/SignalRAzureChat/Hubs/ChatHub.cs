862ae65674f2e75461e30bb8bae3e6c5bcbe162e
fjvela <fjvela@gmail.com>
2018-10-14 16:48:28 +0200

    Init azure signalR service.

---
+++ SignalRAzureChat/SignalRAzureChat/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRAzureChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public void SendNewUser(string user)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.newChatMsg($"{DateTime.Now} The {user} connected!");
+        }
+
+        public void Send(string user, string msg)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.newChatMsg($"{DateTime.Now} {user}: {msg}");
+        }
+    }
+}
